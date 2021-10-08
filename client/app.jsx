83ae369a89005980983f4666800ca829df4a85b2
Chris Difiore <christopher.difiore@gmail.com>
2021-10-07 18:28:52 -0700

    Styling/Design Added

--- client/app.jsx
+++ client/app.jsx
@@ -2,11 +2,23 @@
 import React from 'react';
 import { parseRoute } from './lib';
 import Home from './pages/home';
-import Loginpage from './pages/loginpage';
 import SignupPage from './pages/signupPage';
+import { Switch, Route, Link, BrowserRouter as Router, BrowserRouter } from 'react-router-dom';
+import LoginPage from './pages/loginpage';
+import Signup from './components/signup';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+      <div className="ui container">
+        <BrowserRouter>
+        <div>
+          <Home />
+          <Route path='/' exact Component={Home} />
+          <Route path='/api/auth/sign-up' exact Component={Signup} />
+        </div>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
